a1dc724d8c2edeaa122c40448b17133ec3dcce38
Amir Mirgaliev <amirkek80@gmail.com>
2025-05-28 23:28:27 +0500

    Стилистичекий правки

--- components/lessons/RouletteCard.vue
+++ components/lessons/RouletteCard.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class="lesson-card rounded-xl bg-[#91b377] cursor-pointer" @click="startRoulette">
-		<img src="/images/roulette.jpg" class="rounded-xl w-full h-auto max-h-56 object-contain" />
+	<div
+		class="lesson-card flex items-center justify-center rounded-xl bg-[#91b377] cursor-pointer"
+		@click="startRoulette"
+	>
+		<img src="/images/roulette.png" class="rounded-xl w-36 h-auto object-contain" />
 		<!-- <div class="p-3 text-center">
 			<p class="text-lg font-bold">🎲 Random Game</p>
 			<p class="text-sm text-gray-500">{{ count }} rounds</p>
